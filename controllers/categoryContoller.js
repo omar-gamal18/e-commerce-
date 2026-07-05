75f08f98ecf0180ef63d16ca1c00c730b4b53d5c
Omar Gamal <omar.gamal18200588@gmail.com>
2026-07-05 12:54:05 +0300

    feat(category): create model and controller

---
+++ controllers/categoryContoller.js
@@ -0,0 +1,88 @@
+const slugify = require("slugify");
+
+const Category = require("../models/categoryModel");
+
+exports.getAllCategories = async (req, res) => {
+  const page = +req.query.page || 1;
+  const limit = +req.query.limit || 10;
+  const skip = (page - 1) * limit;
+
+  const categories = await Category.find().skip(skip).limit(limit);
+
+  res.status(200).json({
+    status: "success",
+    results: categories.length,
+    data: { categories },
+  });
+};
+
+exports.getCategory = async (req, res) => {
+  const category = await Category.findById(req.params.id);
+
+  if (!category) {
+    return res.status(404).json({
+      status: "fail",
+      message: "No Category With This Id",
+    });
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: { category },
+  });
+};
+
+exports.createCategory = async (req, res) => {
+  const { name } = req.body;
+
+  const category = await Category.create({
+    name,
+    slug: slugify(name),
+  });
+
+  res.status(201).json({
+    status: "success",
+    data: { category },
+  });
+};
+
+exports.updateCategory = async (req, res) => {
+  const { name } = req.body;
+
+  const category = await Category.findByIdAndUpdate(
+    req.params.id,
+    { name, slug: slugify(name) },
+    {
+      new: true,
+      runValidators: true,
+    },
+  );
+
+  if (!category) {
+    return res.status(404).json({
+      status: "fail",
+      message: "No Category With This Id",
+    });
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: { category },
+  });
+};
+
+exports.deleteCategory = async (req, res) => {
+  const category = await Category.findByIdAndDelete(req.params.id);
+
+  if (!category) {
+    return res.status(404).json({
+      status: "fail",
+      message: "No Category With This Id",
+    });
+  }
+
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+};
