467cda9a475120ee74b02fa6ba366650c4fa0b52
Omar Gamal <omar.gamal18200588@gmail.com>
2026-07-05 19:10:32 +0300

    create api error and global error

---
+++ utils/apiError.js
@@ -0,0 +1,10 @@
+class ApiError extends error {
+  constructor(message, statusCode) {
+    super(message);
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith(4) ? "fail" : "error";
+    this.isOperational = true;
+  }
+}
+
+module.exports = ApiError;
