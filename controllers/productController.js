54a59762d0601d37860474c39b71f98e0f9a6ea8
Omar Gamal <omar.gamal18200588@gmail.com>
2026-07-14 02:39:08 +0300

    apply sort,search and limit fields

--- controllers/productController.js
+++ controllers/productController.js
@@ -6,7 +6,7 @@ const Product = require("../models/productModel");
 exports.getProducts = async (req, res) => {
   // eslint-disable-next-line node/no-unsupported-features/es-syntax
   const queryStrObj = { ...req.query };
-  const excludesFields = ["sort", "limit", "page", "fields"];
+  const excludesFields = ["sort", "limit", "page", "fields", "keyword"];
   excludesFields.forEach((field) => delete queryStrObj[field]);
 
   // Apply filtration using [gte, gt, lte, lt]
@@ -17,10 +17,34 @@ exports.getProducts = async (req, res) => {
   const limit = req.query.limit * 1 || 20;
   const skip = (page - 1) * limit;
 
-  const mongooseQuery = Product.find(JSON.parse(queryStr))
+  let mongooseQuery = Product.find(JSON.parse(queryStr))
     .skip(skip)
     .limit(limit)
     .populate({ path: "category", select: "name -_id" });
+
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(",").join(" ");
+    mongooseQuery = mongooseQuery.sort(sortBy);
+  } else {
+    mongooseQuery = mongooseQuery.sort("-createdAt");
+  }
+
+  if (req.query.fields) {
+    const field = req.query.fields.split(",").join(" ");
+    mongooseQuery = mongooseQuery.select(field);
+  } else {
+    mongooseQuery = mongooseQuery.select("-__v");
+  }
+
+  if (req.query.keyword) {
+    const query = {};
+    query.$or = [
+      { title: { $regex: req.query.keyword, $options: "i" } },
+      { description: { $regex: req.query.keyword, $options: "i" } },
+    ];
+
+    mongooseQuery = mongooseQuery.find(query);
+  }
 
   const products = await mongooseQuery;
 
