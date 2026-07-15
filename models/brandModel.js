37e9a5da6f233facfad772d6597d0ef78cace6f0
Omar Gamal <omar.gamal18200588@gmail.com>
2026-07-15 21:12:03 +0300

    return image url to the response

--- models/brandModel.js
+++ models/brandModel.js
@@ -18,6 +18,22 @@ const BrandSchema = mongoose.Schema(
   { timestamps: true },
 );
 
+const setImageURL = (doc) => {
+  if (doc.image) {
+    const imageUrl = `${process.env.BASE_URL}/brands/${doc.image}`;
+    doc.image = imageUrl;
+  }
+};
+// findOne, findAll and update
+BrandSchema.post("init", (doc) => {
+  setImageURL(doc);
+});
+
+// create
+BrandSchema.post("save", (doc) => {
+  setImageURL(doc);
+});
+
 const Brand = mongoose.model("Brand", BrandSchema);
 
 module.exports = Brand;
--- models/categoryModel.js
+++ models/categoryModel.js
@@ -18,6 +18,22 @@ const categorySchema = mongoose.Schema(
   { timestamps: true },
 );
 
+const setImageURL = (doc) => {
+  if (doc.image) {
+    const imageUrl = `${process.env.BASE_URL}/categories/${doc.image}`;
+    doc.image = imageUrl;
+  }
+};
+// findOne, findAll and update
+categorySchema.post("init", (doc) => {
+  setImageURL(doc);
+});
+
+// create
+categorySchema.post("save", (doc) => {
+  setImageURL(doc);
+});
+
 const Category = mongoose.model("Category", categorySchema);
 
 module.exports = Category;
