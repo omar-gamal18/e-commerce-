22c45778a8c1ac5d6b290cbfef52fa200defa96b
Omar Gamal <omar.gamal18200588@gmail.com>
2026-07-15 21:14:53 +0300

    enhance error middleware

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -1,47 +1,25 @@
-const ApiError = require("../utils/apiError");
-
-const handleCastErrorDB = (err) => {
-  return new ApiError(`Invalid path ${err.path}: ${err.value}`, 400);
-};
-
-const globalError = (err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
-
-  if (process.env.NODE_ENV === "development") {
-    sendErrorForDev(err, res);
-  } else {
-    let error = { ...err };
-    error.message = err.message;
-
-    if (err.name === "CastError") error = handleCastErrorDB(error);
-
-    sendErrorForProd(error, res);
-  }
-};
-
-const sendErrorForDev = (err, res) => {
-  return res.status(err.statusCode).json({
+const sendErrorForDev = (err, res) =>
+  res.status(err.statusCode).json({
     status: err.status,
     error: err,
     message: err.message,
     stack: err.stack,
   });
-};
 
-const sendErrorForProd = (err, res) => {
-  // Operational, trusted error: send message to client
-  if (err.isOperational) {
-    return res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message,
-    });
-  }
-  // Programming or other unknown error: don't leak error details
-  return res.status(500).json({
-    status: "error",
-    message: "Something went wrong!",
+const sendErrorForProd = (err, res) =>
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
+
+const globalError = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+  if (process.env.NODE_ENV === "development") {
+    sendErrorForDev(err, res);
+  } else {
+    sendErrorForProd(err, res);
+  }
 };
 
 module.exports = globalError;
