b4618780c12c5574e5073da0f69b83f43b784c8f
Omar Gamal <omar.gamal18200588@gmail.com>
2026-07-16 17:40:56 +0300

    create user schema

---
+++ models/userModel.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: [true, "name required"],
+    },
+    slug: {
+      type: String,
+      lowercase: true,
+    },
+    email: {
+      type: String,
+      lowercase: true,
+      unique: true,
+      required: [true, "email required"],
+    },
+    phone: String,
+    profileImg: String,
+
+    password: {
+      type: String,
+      required: [true, "password required"],
+      minlength: [6, "too short password"],
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+  },
+  { timestamps: true },
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
