e41c51ba8a606701c777fb28f81cc30a1ffae188
Omar Gamal <omar.gamal18200588@gmail.com>
2026-07-23 15:51:09 +0300

    feat(Cart): cart schema

---
+++ models/cartModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const cartSchema = mongoose.Schema(
+  {
+    cartItems: [
+      {
+        product: {
+          type: mongoose.Schema.ObjectId,
+          ref: "Product",
+        },
+        quantity: Number,
+        color: String,
+        price: Number,
+      },
+    ],
+    totalcartPrice: Number,
+    totalPriceAfterDiscount: Number,
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+    },
+  },
+
+  { timestamps: true },
+);
+
+const Cart = mongoose.model("Cart", cartSchema);
+
+module.exports = Cart;
