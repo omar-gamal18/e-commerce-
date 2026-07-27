cfccd3b6250dc6964d79318f406ed434fc23f278
Omar Gamal <omar.gamal18200588@gmail.com>
2026-07-27 17:09:23 +0300

    enable CORS and compression respons

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const path = require("path");
 
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
+const compression = require("compression");
 
 require("dotenv").config();
 const globalError = require("./middlewares/errorMiddleware");
@@ -9,6 +11,11 @@ const ApiError = require("./utils/apiError");
 const mountRoutes = require("./routes");
 
 const app = express();
+app.use(cors());
+app.options("*", cors());
+
+app.use(compression());
+
 app.set("query parser", "extended");
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "uploads")));
