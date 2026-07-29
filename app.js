d6048b4547ce2274e4b7e04bf449aa6a6d7eba27
Omar Gamal <omar.gamal18200588@gmail.com>
2026-07-29 23:39:29 +0300

    enhance security

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const compression = require("compression");
+const rateLimit = require("express-rate-limit");
+const hpp = require("hpp");
 
 require("dotenv").config();
 const globalError = require("./middlewares/errorMiddleware");
@@ -12,25 +14,41 @@ const mountRoutes = require("./routes");
 const { webhookCheckout } = require("./controllers/orderController");
 
 const app = express();
+
 app.use(cors());
 app.options(/.*/, cors());
 
 app.use(compression());
 
-app.post(
-  "/webhook-checkout",
-  express.raw({ type: "*/*" }),
-  webhookCheckout,
-);
+app.post("/webhook-checkout", express.raw({ type: "*/*" }), webhookCheckout);
 
 app.set("query parser", "extended");
-app.use(express.json());
+app.use(express.json({ limit: "10kb" }));
 app.use(express.static(path.join(__dirname, "uploads")));
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
   console.log(`mode: ${process.env.NODE_ENV}`);
 }
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  message: "Too many requests from this IP, please try again after 15 minutes",
+});
+app.use(limiter);
+app.use(
+  hpp({
+    whitelist: [
+      "price",
+      "ratingsAverage",
+      "ratingsQuantity",
+      "sold",
+      "quantity",
+    ],
+  }),
+);
 
 mountRoutes(app);
 
